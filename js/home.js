1ea76ce35a4850bec7f21c76900f15c723394606
Jahid458 <jahidul898@outlook.com>
2024-09-19 12:11:38 +0600

    partially  added money event handeler

--- js/home.js
+++ js/home.js
@@ -1,1 +1,20 @@
-console.log('Home added')
+//add money to the account
+
+/**
+ * s-1:add event handeler
+ * prevent page reload after form submit
+ * s-2: get money to be added to the account balance
+ */
+//step-1: add an event handeler to the add money  button inside the form
+document.getElementById('btn-add-money')
+    .addEventListener('click', function(event){
+      //prevent page 
+      event.preventDefault();
+      //step-2: get money to be added the account
+      const addMoneyInput = document.getElementById('input-add-money').value;
+      console.log(addMoneyInput);
+      //get the pin number provide 
+      const pinNumberInput = document.getElementById('input-pin-number').value;
+      console.log(pinNumberInput)
+
+    })
